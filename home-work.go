1420cd99c7deed80c3fb1e7da2eef81c6787cf82
cryptokitty <19489650+injectz@users.noreply.github.com>
2019-11-18 01:51:59 +0300

    done second home-work

---
+++ home-work.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+// Task #1
+func factorial(i uint) uint {
+	sum := 1
+	for z := 1; z <= int(i); z++ {
+		sum *= z
+	}
+	return uint(sum)
+}
+
+// Task #2
+type Point struct {
+	x, y int
+}
+
+type Square struct {
+	start Point
+	a     uint
+}
+
+func (s Square) End() Point {
+	return Point{int(s.a), int(s.a)}
+}
+
+func (s Square) Perimeter() uint {
+	return s.a * 4
+}
+
+func (s Square) Area() uint {
+	return s.a * s.a
+}
+
+func main() {
+	fmt.Println("Factorial 10 =", factorial(5))
+
+	s := Square{Point{1, 1}, 5}
+	fmt.Println("Square end point:", s.End())
+	fmt.Println("Square perimeter =", s.Perimeter())
+	fmt.Println("Square area =", s.Area())
+}
