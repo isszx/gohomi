c7d04aa2ce04ab440ecaabd08c83881886797a56
cryptokitty <19489650+injectz@users.noreply.github.com>
2019-12-13 01:57:19 +0300

    remove all custom magic :/

--- task1/task1.go
+++ task1/task1.go
@@ -2,41 +2,10 @@ package task1
 
 import (
 	"strconv"
-	"unicode/utf8"
 )
 
-func contains(val string) bool {
-	for _, a := range []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"} {
-		if val == a {
-			return true
-		}
-	}
-	return false
-}
-
-func getIntStr(s string) string {
-	if utf8.RuneCountInString(s) == 0 {
-		return "0"
-	}
-	res := ""
-	for _, r := range s {
-		if contains(string(r)) {
-			res = res + string(r)
-			continue
-		}
-		if !contains(string(r)) && utf8.RuneCountInString(res) == 0 {
-			continue
-		}
-		if !contains(string(r)) && utf8.RuneCountInString(res) > 0 {
-			return res
-		}
-	}
-	return res
-}
-
 func StrToInt(s string) (int, error) {
-	line := getIntStr(s)
-	v, err := strconv.Atoi(line)
+	v, err := strconv.Atoi(s)
 	if err != nil {
 		return 0, err
 	}
--- task1/task1_test.go
+++ task1/task1_test.go
@@ -11,20 +11,23 @@ func TestInt(t *testing.T) {
 	assert.Nil(t, e, r)
 }
 
-func TestFloat(t *testing.T) {
-	r, e := StrToInt("1.0")
+func TestInt2(t *testing.T) {
+	r, e := StrToInt("01")
 	assert.Equal(t, 1, r, e)
 	assert.Nil(t, e, r)
+}
+
+func TestFloat(t *testing.T) {
+	r, e := StrToInt("1.0")
+	assert.NotNil(t, e, r)
 }
 
 func TestText(t *testing.T) {
 	r, e := StrToInt("z11a")
-	assert.Equal(t, 11, r, e)
-	assert.Nil(t, e, r)
+	assert.NotNil(t, e, r)
 }
 
 func TestSpaces(t *testing.T) {
 	r, e := StrToInt("1 2 3")
-	assert.Equal(t, 1, r, e)
-	assert.Nil(t, e, r)
+	assert.NotNil(t, e, r)
 }
